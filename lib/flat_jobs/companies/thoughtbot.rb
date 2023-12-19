d3aade6cd44d2b49071c80fb5d9ebe7a9367d939
Michael Winton <wintonmr@gmail.com>
2023-12-19 11:26:17 +0100

    Add Thoughtbot

---
+++ lib/flat_jobs/companies/thoughtbot.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module FlatJobs
+  module Companies
+    class Thoughtbot < FlatJobs::Company
+      FlatJobs.register_company(:thoughtbot, new)
+
+      def fetch_data(data_path: "section#jobs")
+        response = FlatJobs::Client.get(**REQUEST_OPTS)
+        doc = Nokogiri::HTML(response.body)
+        data_element = doc.at_css(data_path)
+
+        if data_element.present?
+          data_element.content
+        else
+          raise FlatJobs::Error.new("Data element not found")
+        end
+      end
+
+      def data_file_type
+        FileType::HTML
+      end
+
+      def parse_jobs(data)
+        jobs_empty = data.include?("We currently have no open positions.")
+        jobs_count = jobs_empty ? 0 : 1
+
+        FlatJobs::WipJob.new(company: company_name, count: jobs_count).to_csv
+      end
+
+      REQUEST_OPTS = {
+        url: "https://thoughtbot.com/jobs",
+        params: {},
+        headers: {
+          "Content-Type" => "text/html"
+        }
+      }.freeze
+      private_constant :REQUEST_OPTS
+    end
+  end
+end
---
+++ spec/flat_jobs/companies/thoughtbot_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+RSpec.describe FlatJobs::Companies::Thoughtbot do
+  describe "#fetch_data", vcr: "thoughtbot_data" do
+    it "returns data" do
+      result = FlatJobs::Companies::Thoughtbot.new.fetch_data
+
+      expect(result).not_to be_empty
+    end
+
+    it "raises an error when no data element is found" do
+      expect do
+        FlatJobs::Companies::Thoughtbot.new.fetch_data(data_path: "random_path")
+      end.to raise_error(FlatJobs::Error, "Data element not found")
+    end
+  end
+
+  describe "#data_file_type" do
+    it "returns expected file type" do
+      result = FlatJobs::Companies::Thoughtbot.new.data_file_type
+
+      expect(result).to eq(FlatJobs::FileType::HTML)
+    end
+  end
+
+  describe "#parse_jobs" do
+    it "reports 0 jobs when no jobs are returned" do
+      data = vcr_response_data(vcr: "thoughtbot_data").first
+
+      result = FlatJobs::Companies::Thoughtbot.new.parse_jobs(data)
+
+      expect(result).to eq("thoughtbot,-,-,-,-,0 jobs found\n")
+    end
+
+    it "reports jobs when no-jobs-message is not found" do
+      data = "We have open positions"
+
+      result = FlatJobs::Companies::Thoughtbot.new.parse_jobs(data)
+
+      expect(result).to eq("thoughtbot,-,-,-,-,1 jobs found\n")
+    end
+  end
+end
