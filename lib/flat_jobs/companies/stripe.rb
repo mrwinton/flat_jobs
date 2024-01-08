27f003835c6f97d2a85c59ceadb6ebc568ce0c26
Michael Winton <wintonmr@gmail.com>
2024-01-08 20:38:43 +0100

    Add Stripe

---
+++ lib/flat_jobs/companies/stripe.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module FlatJobs
+  module Companies
+    class Stripe < FlatJobs::Company
+      FlatJobs.register_company(:stripe, new)
+
+      def fetch_data
+        FlatJobs::Client.get(**REQUEST_OPTS).body
+      end
+
+      def data_file_type
+        FileType::HTML
+      end
+
+      def parse_jobs(data)
+        doc = Nokogiri::HTML(data)
+        doc.css(".JobsListings__tableBody .TableRow").map { |job| parse_job(job) }
+      end
+
+      private
+
+      STRIPE_URL = "https://stripe.com"
+      private_constant :STRIPE_URL
+
+      REQUEST_OPTS = {
+        url: "#{STRIPE_URL}/jobs/search?teams=Banking+as+a+Service&teams=Climate&teams=Connect&teams=New+Financial+Products&teams=Payments&teams=Platform&teams=Revenue+%26+Financial+Automation&teams=Tax&teams=Terminal&remote_locations=Europe--Sweden+Remote",
+        params: {},
+        headers: {
+          "Content-Type" => "text/html"
+        }
+      }.freeze
+      private_constant :REQUEST_OPTS
+
+      def parse_job(job)
+        job_title_element = job.at_css(".JobsListings__tableCell--title a")
+        job_url = STRIPE_URL + job_title_element["href"]
+        job_id = job_url.split("/").last
+        job_title = job_title_element.content
+        job_location_element = job.at_css(".JobsListings__locationDisplayName")
+        job_location = job_location_element.content
+
+        FlatJobs::Job.new(
+          company: company_name,
+          id: job_id,
+          title: job_title,
+          location: job_location,
+          url: job_url,
+          notes: ""
+        )
+      end
+    end
+  end
+end
---
+++ spec/flat_jobs/companies/stripe_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+RSpec.describe FlatJobs::Companies::Stripe do
+  describe "#fetch_data", vcr: "stripe_data" do
+    it "returns data" do
+      result = FlatJobs::Companies::Stripe.new.fetch_data
+
+      expect(result).not_to be_empty
+    end
+  end
+
+  describe "#data_file_type" do
+    it "returns expected file type" do
+      result = FlatJobs::Companies::Stripe.new.data_file_type
+
+      expect(result).to eq(FlatJobs::FileType::HTML)
+    end
+  end
+
+  describe "#parse_jobs" do
+    it "returns jobs" do
+      data = vcr_response_data(vcr: "stripe_data").first
+
+      result = FlatJobs::Companies::Stripe.new.parse_jobs(data)
+
+      expect(result.count).not_to be_zero
+      job = result.first
+      expect(job.company).to eq("stripe")
+      expect(job.id).to eq("5563735")
+      expect(job.title).to eq("Backend Engineer, High Availability")
+      expect(job.url).to eq("https://stripe.com/jobs/listing/backend-engineer-high-availability/5563735")
+      expect(job.location).to eq("Seattle")
+      expect(job.notes).to eq("")
+    end
+  end
+end
